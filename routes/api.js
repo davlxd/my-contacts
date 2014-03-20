5174b970a2c3d7e3ec5face7b9a4e5944032ea66
lxd <xdli@thoughtworks.com>
2014-03-20 19:49:44 +0800

    Add submodule

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,27 @@
 /*
  * Serve JSON to our AngularJS client
  */
+var fs = require('fs');
+var path = require('path');
 
-var data = {
-    'contacts': [
-	{
-	    'name': 'lxd',
-	    'events': 'birthday'
-	},
-	{
-	    'name': 'mei',
-	    'events': 'birthday birthday'
-	},
-    ]
-};
+
+var data = {};
+exports.inspect_data = function() {
+  return data;
+}
+
+var data_file_name = function() {
+  return __dirname + '/../data/my-contacts.json';
+}
+var load = function() {
+  data = JSON.parse(fs.readFileSync(data_file_name(), 'utf8'));
+}
+var save = function() {
+  fs.writeFile(data_file_name(), JSON.stringify(data), 'utf8');
+}
+var sync = function() {
+  Object.keys(data).length === 0 ? load() : save();
+}
 
 exports.name = function (req, res) {
   res.json({
@@ -23,58 +31,66 @@ exports.name = function (req, res) {
 
 //GET
 exports.contacts = function (req, res) {
-    var contacts = [];
-    data.contacts.forEach(function (contact, i) {
-	contacts.push({
-	    id: i,
-	    name: contact.name,
-	    events: contact.events.substr(0, 10) + '...'
-	});
+  sync();
+  var contacts = [];
+  data.contacts.forEach(function (contact, i) {
+    contacts.push({
+      id: i,
+      name: contact.name,
+      events: contact.events.substr(0, 10) + '...'
     });
-    res.json({
-	contacts: contacts
-    });
+  });
+  res.json({
+    contacts: contacts
+  });
 };
 
 //GET
 exports.contact = function (req, res) {
-    var id = req.params.id;
-    if (id >= 0 && id < data.contacts.length) {
-	res.json({
-	    contact: data.contacts[id]
-	});
+  sync();
+  var id = req.params.id;
+  if (id >= 0 && id < data.contacts.length) {
+    res.json({
+      contact: data.contacts[id]
+    });
 
-    } else {
-	res.json(false);
-    }
+  } else {
+    res.json(false);
+  }
 };
 
 //POST
 exports.addContact = function(req, res) {
-    data.contacts.push(req.body);
-    res.json(data.contacts.length - 1);
+  sync();
+  data.contacts.push(req.body);
+  res.json(data.contacts.length - 1);
+  sync();
 };
 
 //PUT
 exports.editContact = function(req, res) {
-    var id = req.params.id;
-    if (id >= 0 && id < data.contacts.length) {
-	data.contacts[id] = req.body;
-	res.json(true);
+  sync();
+  var id = req.params.id;
+  if (id >= 0 && id < data.contacts.length) {
+    data.contacts[id] = req.body;
+    res.json(true);
 
-    } else {
-	res.json(false);
-    }
+  } else {
+    res.json(false);
+  }
+  sync();
 };
 
 
 //DELETE
 exports.deleteContact = function (req, res) {
-    var id = req.params.id;
-    if (id >=0 && id < data.contacts.length) {
-	data.contacts.splice(id, 1)
-	res.json(true);
-    } else {
-	res.json(false);
-    }
+  sync();
+  var id = req.params.id;
+  if (id >=0 && id < data.contacts.length) {
+    data.contacts.splice(id, 1)
+    res.json(true);
+  } else {
+    res.json(false);
+  }
+  sync();
 };
