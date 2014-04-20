3b0b49a59cb97d1ac7ef7b6198634f2936926a56
lxd <i@lxd.me>
2014-04-20 14:47:01 +0800

    Will fail if mouse move too fast

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,12 +1,91 @@
-angular.module('myAnimations', [
+var myAnimations = angular.module('myAnimations', [
   'ngAnimate'
-]).
-  animation('.contact-card', function() {
-    var animateUp = function(element, className, done) {}
-    var animateDown = function(element, className, done) {}
+]);
+
+myAnimations.animation('.contact-card', function() {
+    var animateUp = function(element, className, done) {
+      console.log('.contact-card'  + element);
+      if(className != 'expand') {
+	return ;
+      }
+      if(jQuery(element).is(":animated")) {
+      	return ;
+      }
+      jQuery(element).animate({
+        height: 60
+      }, 200, done);
+      return function(cancel) {
+	if(cancel) {
+	  element.stop();
+	}
+      };
+    }
+    var animateDown = function(element, className, done) {
+      console.log('.contact-card'  + element);
+      if(className != 'expand') {
+	return;
+      }
+      if(jQuery(element).is(":animated")) {
+      	return ;
+      }
+
+      jQuery(element).animate({
+        height: 20
+      }, 200, done);
+
+      return function(cancel) {
+	if(cancel) {
+	  element.stop();
+	}
+      };
+    }
 
     return {
       addClass: animateUp,
       removeClass: animateDown
     };
   });
+
+myAnimations.animation('.detail', function() {
+  console.log('.detail general');
+    var animateUp = function(element, className, done) {
+      console.log('.detail'  + element);
+      if(className != 'active') {
+	return;
+      }
+      if(jQuery(element).is(":animated")) {
+      	return ;
+      }
+      jQuery(element).show(200);
+
+      return function(cancel) {
+      	if(cancel) {
+      	  element.stop();
+//    	  element.hide(200);
+      	}
+      };
+    }
+    var animateDown = function(element, className, done) {
+      console.log('.detail'  + element);
+      if(className != 'active') {
+	return;
+      }
+      if(jQuery(element).is(":animated")) {
+      	return ;
+      }
+      jQuery(element).hide(200);
+
+      return function(cancel) {
+	if(cancel) {
+	  element.stop();
+	  element.hide(200);
+	}
+      };
+    }
+
+    return {
+      addClass: animateUp,
+      removeClass: animateDown
+    };
+  });
+
